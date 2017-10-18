5f2d038cd83bcb160db7b93cac65867c35982a21
Vagrant Default User <vagrant@jessie.vagrantup.com>
2017-10-18 14:02:28 +0000

    Creating migrations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    ActiveRecord::Base.establish_connection(
+      :adapter  => "postgresql",
+      :host     => "localhost",
+      :username => "postgres",
+      :password => "vagrant",
+      :database => "myrubyblog"
+    )
   end
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+  def index
+    @content_first = 'First Line'
+    @content_second = 'Second Line'
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
