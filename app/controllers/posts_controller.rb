9cd97aaedba2fb089bcfa9265a9676272021f9f4
Vagrant Default User <vagrant@jessie.vagrantup.com>
2017-10-20 20:07:48 +0000

    CRUD created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,50 @@
 class PostsController < ApplicationController
 
   def index
-    @content_first = 'First Line'
-    @content_second = 'Second Line'
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
 
+  def new
+    @post = Post.new
+    @category = Category.all
+  end
+
+  def create
+    @post = Post.create(ad_params)
+    if @post.save
+      redirect_to posts_path, :notice => "Your post has been saved"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(ad_params)
+      redirect_to posts_path, :notice => "Your post has been updated"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path, :notice => "Your post has been deleted"
+    else
+      render "edit"
+    end
+  end
 
+  def ad_params
+    params.require(:post).permit(:title, :body, :category_id)
   end
 end
